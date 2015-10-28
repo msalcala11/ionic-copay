14b26f0ab5647f8449a8d25460c6c2647c167f91
Jason Dreyzehner <jason@bitpay.com>
2015-10-28 18:50:47 -0400

    now it's just v2

--- www/views/action/action.js
+++ www/views/action/action.js
@@ -1,4 +1,4 @@
-angular.module('copayApp')
+angular.module('app')
   .controller('ActionCtrl', function($scope) {
     // With Ionic view caching, controllers are only run on app start
     $scope.$on('$ionicView.enter', function(e) {
