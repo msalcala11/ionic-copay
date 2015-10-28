53c52f731cf17c77c967ddc4eabaee071ce429b6
Marty Alcala <marty@bitpay.com>
2015-10-28 17:29:42 -0400

    got remote file download, load, and delete working

--- www/views/overview/overview.js
+++ www/views/overview/overview.js
@@ -1,41 +1,183 @@
 angular.module('copayApp')
-  .controller('OverviewCtrl', function($scope, $cordovaFileTransfer){//, $cordovaFile) {
+  .controller('OverviewCtrl', function($scope, $cordovaFileTransfer, $cordovaFile) {
     // With Ionic view caching, controllers are only run on app start
     $scope.downloadFile = function() {
       console.log('$scope.downloadFile called');
-      var url = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js";
-      var filename = url.split("/").pop();
-      alert(filename);
-      var targetPath = cordova.file.externalRootDirectory + filename;
-      var trustHosts = true
-      var options = {};
-      alert(cordova.file.externalRootDirectory);
-      console.log('targetPath', targetPath);
-      $cordovaFileTransfer.download(url, targetPath, options, trustHosts)
-        .then(function(result) {
-          // Success!
-          alert(JSON.stringify(result));
-        }, function(error) {
-          // Error
-          alert(JSON.stringify(error));
-        }, function (progress) {
-          $timeout(function () {
-            $scope.downloadProgress = (progress.loaded / progress.total) * 100;
-          })
-        });
+      window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fs) {
+        fs.root.getDirectory(
+            "ExampleProject",
+            {
+                create: true
+            },
+            function(dirEntry) {
+                dirEntry.getFile(
+                    "test.png",
+                    {
+                        create: true,
+                        exclusive: false
+                    },
+                    function gotFileEntry(fe) {
+                        var p = fe.toURL();
+                        fe.remove();
+                        ft = new FileTransfer();
+                        ft.download(
+                            encodeURI("https://bitpay.com/images/bitcoin.19154c52.svg"),
+                            p,
+                            function(entry) {
+                                console.log('entry', entry);
+                                //$ionicLoading.hide();
+                                $scope.imgFile = entry.toURL();
+                                console.log('$scope.imgFile', $scope.imgFile);
+                            },
+                            function(error) {
+                                //$ionicLoading.hide();
+                                console.log('error', JSON.stringify(error));
+                                alert("Download Error Source -> " + error.source);
+                            },
+                            false,
+                            null
+                        );
+                    },
+                    function() {
+                        //$ionicLoading.hide();
+                        console.log("Get file failed");
+                    }
+                );
+            }
+        );
+    },
+    function() {
+        //$ionicLoading.hide();
+        console.log("Request for filesystem failed");
+    });
+      // var url = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js";
+      // var filename = url.split("/").pop();
+      // alert(filename);
+      // var targetPath = cordova.file.externalRootDirectory + filename;
+      // var trustHosts = true
+      // var options = {};
+      // alert(cordova.file.externalRootDirectory);
+      // console.log('targetPath', targetPath);
+      // $cordovaFileTransfer.download(url, targetPath, options, trustHosts)
+      //   .then(function(result) {
+      //     // Success!
+      //     alert(JSON.stringify(result));
+      //   }, function(error) {
+      //     // Error
+      //     alert(JSON.stringify(error));
+      //   }, function (progress) {
+      //     $timeout(function () {
+      //       $scope.downloadProgress = (progress.loaded / progress.total) * 100;
+      //     })
+      //   });
     };
 
-    // $cordovaFile.removeFile(filepath)
-    //   .then(function(result) {
-    //           alert("success");
-    //           // Success!
-    //   }, function(err) {
-    //           alert(JSON.stringify(err));
-    //           // Error
-    //   });
+    var filePath;
+    $scope.loadFile = function(){
+      window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fs) {
+          fs.root.getDirectory(
+              "ExampleProject",
+              {
+                  create: false
+              },
+              function(dirEntry) {
+                  dirEntry.getFile(
+                      "test.png",
+                      {
+                          create: false,
+                          exclusive: false
+                      },
+                      function gotFileEntry(fe) {
+                          //$ionicLoading.hide();
+                          $scope.imgFile = fe.toURL();
+                          console.log('loaded file: ', $scope.imgFile);
+                          filePath = $scope.imgFile;
+                      },
+                      function(error) {
+                          //$ionicLoading.hide();
+                          console.log("Error getting file");
+                      }
+                  );
+              }
+          );
+      },
+      function() {
+          $ionicLoading.hide();
+          console.log("Error requesting filesystem");
+      });
+    };
 
+    $scope.deleteFile = function(){
+      var filepath = filePath;
+
+      window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fs) {
+          fs.root.getDirectory(
+              "ExampleProject",
+              {
+                  create: false
+              },
+              function(dirEntry) {
+                  dirEntry.getFile(
+                      "test.png",
+                      {
+                          create: false,
+                          exclusive: false
+                      },
+                      function gotFileEntry(fe) {
+                          //$ionicLoading.hide();
+                          $scope.imgFile = fe.toURL();
+                          console.log('loaded file: ', $scope.imgFile);
+                          filePath = $scope.imgFile;
+
+                          console.log('deleting file');
+                          fe.remove(function(file){
+                            console.log('file removed!')
+                          }, function(error){
+                            console.log('error deleting file: ', error);
+                          }, function(){
+                            console.log('file does not exist')
+                          })
+                      },
+                      function(error) {
+                          //$ionicLoading.hide();
+                          console.log("Error getting file");
+                      }
+                  );
+              }
+          );
+      },
+      function() {
+          $ionicLoading.hide();
+          console.log("Error requesting filesystem");
+      });
+
+      // window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem){
+      //     fileSystem.root.getFile(relativeFilePath, {create:false}, function(fileEntry){
+      //         fileEntry.remove(function(file){
+      //             console.log("File removed!");
+      //         },function(){
+      //             console.log("error deleting the file " + error.code);
+      //             });
+      //         },function(){
+      //             console.log("file does not exist");
+      //         });
+      //     },function(evt){
+      //         console.log(evt.target.error.code);
+      // });
+
+
+
+      // $cordovaFile.removeFile(filepath)
+      //   .then(function(result) {
+      //           alert("success");
+      //           // Success!
+      //   }, function(err) {
+      //           alert(JSON.stringify(err));
+      //           // Error
+      //   });
+    };
 
     $scope.$on('$ionicView.enter', function(e) {
-      $scope.downloadFile();
+
     });
   });
